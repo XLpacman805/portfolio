3e73a6cb6ea8d13db093db1182747a9bfd97fcca
Johnny Meza <xlpacman805@gmail.com>
2020-03-22 14:13:51 -0700

    wrote constructor and render function

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class Navigation extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
 
+    render() {
+        return(
+            <div>
+                <h1>Navigation Component</h1>
+            </div>
+        );
+    }
 }
 
 export default Navigation;
