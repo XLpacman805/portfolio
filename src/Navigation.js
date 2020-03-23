7e347ed2f8f5826f1dceb6d4dee01f434bf941d0
Johnny Meza <xlpacman805@gmail.com>
2020-03-22 18:46:46 -0700

    temporary drawer added aka slide out menu nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import TemporaryDrawer from './TemporaryDrawer';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,13 +29,14 @@ export default function Navigation() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <TemporaryDrawer />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Johnny's Portfolio
           </Typography>
         </Toolbar>
       </AppBar>
+
     </div>
   );
 }
