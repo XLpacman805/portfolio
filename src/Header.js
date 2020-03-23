aa36072fdc37f92cfad118ae0afeb51ef10f53ef
Johnny Meza <xlpacman805@gmail.com>
2020-03-22 19:28:35 -0700

    Started Header Component

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+          </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+          </a>
+            </header>
+        );
+    }
+}
+
+export default Header
