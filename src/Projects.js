421d316982121e1a2210182da144ee25990ac166
Johnny Meza <xlpacman805@gmail.com>
2020-03-23 22:55:58 -0700

    Created media cards

--- src/Projects.js
+++ src/Projects.js
@@ -37,20 +37,75 @@ export default function Projects() {
                     />
                     <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
+                        Exercise Tracker API
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
+                        A REST API written in JavaScript using NodeJS and MongoDB. You can use it to add user, get a list of users,and get a certain users' exercise logs. Those exercise logs can also be filtered by date rang, and response limit. 
                     </Typography>
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
                 <Button size="small" color="primary">
-                Share
+                    Open Application
                 </Button>
                 <Button size="small" color="primary">
-                Learn More
+                    See Code
+                </Button>
+            </CardActions>
+            </Card>
+        </Grid>
+
+        <Grid item xs={12}>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                    className={classes.media}
+                    image="https://via.placeholder.com/500"
+                    title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        URL Shortener Microservice
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        A REST API written in JavaScript using NodeJS and MongoDB. It will take a long url and return a short url. The short url, when visited redirects you to the original website.
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                <Button size="small" color="primary">
+                    Open Application
+                </Button>
+                <Button size="small" color="primary">
+                    See Code
+                </Button>
+            </CardActions>
+            </Card>
+        </Grid>
+
+        <Grid item xs={12}>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                    className={classes.media}
+                    image="https://via.placeholder.com/500"
+                    title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        This Portfolio!
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Written in REACT using the Material-UI component library. I utilize hooks, props, and APIs to make this site. It's also deployed to Google App Engine. I planned it out using Jira, and made wireframes with Invision.
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                <Button size="small" color="primary">
+                    Open wireframes
+                </Button>
+                <Button size="small" color="primary">
+                    See Code
                 </Button>
             </CardActions>
             </Card>
