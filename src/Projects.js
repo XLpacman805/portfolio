425edb21af51d90c62a1703442112dee17331652
Johnny Meza <xlpacman805@gmail.com>
2020-03-23 20:39:34 -0700

    Added card

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-class Projects extends React.Component {
-    render() {
-        return (
-            <h2>Projects Component</h2>
-        );
-    }
+const useStyles = makeStyles({
+  root: {
+      maxWidth: 500,
+  },
+  media: {
+    height: 300,
+  },
+});
+
+export default function Projects() {
+  const classes = useStyles();
+
+  return (
+    <Grid container spacing={1}>
+        <Grid item xs={12}>
+            <h2>Projects</h2>
+        </Grid>
+
+        <Grid item xs={12}>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                    className={classes.media}
+                    image="https://via.placeholder.com/500"
+                    title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                <Button size="small" color="primary">
+                Share
+                </Button>
+                <Button size="small" color="primary">
+                Learn More
+                </Button>
+            </CardActions>
+            </Card>
+        </Grid>
+    </Grid>    
+  );
 }
-
-export default Projects;
