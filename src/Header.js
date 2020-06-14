871042e3af59a63910452aee640f5df8122eb0be
Johnny Meza <xlpacman805@gmail.com>
2020-06-13 18:53:53 -0700

    change resume link

--- src/Header.js
+++ src/Header.js
@@ -54,7 +54,7 @@ export default function Header() {
             </Button>
         </Grid>
         <Grid item xs={5} md={3} xl={1}>
-            <Button variant="contained" color="secondary" size="medium" fullWidth={true} href="https://drive.google.com/open?id=1G8iSJB9kF8VsHWqwJZXdqvSVNEBotq8S" target="_blank">
+            <Button variant="contained" color="secondary" size="medium" fullWidth={true} href="https://drive.google.com/file/d/12A-ihpeSPp5UYyHtXb8zLK6bGq6XlTmL/view?usp=sharing" target="_blank">
                 See Resume
             </Button>
         </Grid>
